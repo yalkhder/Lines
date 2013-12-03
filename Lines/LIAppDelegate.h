f292f4d478ffe9c02f841b1e765dd3d4e247e34a
Yasser Al-Khder <yasser.alkhder@gmail.com>
2013-12-03 00:05:28 -0500

    Initial Commit

---
+++ Lines/LIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LIAppDelegate.h
+//  Lines
+//
+//  Created by Yasser Al-Khder on 12/3/2013.
+//  Copyright (c) 2013 Yasser Al-Khder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lines/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lines
+//
+//  Created by Yasser Al-Khder on 12/3/2013.
+//  Copyright (c) 2013 Yasser Al-Khder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LIAppDelegate class]));
+    }
+}
